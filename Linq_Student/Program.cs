3f91582921e1d40bc25e86447b35426d435463ac
ArsenyKorolkovStudent <94906176+ArsenyKorolkovStudent@users.noreply.github.com>
2022-02-23 11:07:48 +0300

    3

--- Linq_Student/Program.cs
+++ Linq_Student/Program.cs
@@ -22,9 +22,16 @@ namespace Linq_Student
 
         static void Main(string[] args)
         {
-            IEnumerable<Student> studentQuery = 
-                    from student in students
-                    where student.Scores[0] > 90 && student.Scores[3] < 80
+            //Ex1_2();
+            Ex3();
+        }
+
+        static void Ex1_2()
+        {
+            IEnumerable<Student> studentQuery =
+        from student in students
+            //where student.Scores[0] > 90 && student.Scores[3] < 80
+                    orderby student.Scores[0] descending //orderby student.Last ascending
                     select student;
 
             //Такой же пример, но с методом расширения:
@@ -33,7 +40,8 @@ namespace Linq_Student
 
             foreach (Student student in studentQuery)
             {
-                Console.WriteLine("{0}, {1}", student.Last, student.First);
+                //Console.WriteLine("{0}, {1}", student.Last, student.First);
+                Console.WriteLine("{0}, {1} {2}", student.Last, student.First, student.Scores[0]);
             }
 
 
@@ -49,7 +57,80 @@ namespace Linq_Student
             int studentCountW = students.Where(st => st.Scores[0] > 90 && st.Scores[3] < 80).Count();
 
             Console.WriteLine("Количество студентов: {0}, {1}", studentCount, studentCountW);
+
+            //Сохранение результатов запроса в лист (немедленное выполнение):
+
+            var studentList = (
+                from student in students
+                where student.Scores[0] > 90 && student.Scores[3] < 80
+                orderby student.Last ascending
+                select student).ToList();
+
+            foreach (Student student in studentList)
+            {
+                Console.WriteLine("{0}, {1}", student.Last, student.First);
+            }
+
+            Console.WriteLine("---------- Group ----------");
+            //Запрос с предложением group создает последовательность групп, где каждая группа содержит Key
+            //и последовательность, состоящую из всех членов этой группы.
+
+            //Создайте новый запрос, который группирует учащихся по первой букве их фамилии в качестве ключа:
+
+            var studentQuery2 =
+                from student in students
+                group student by student.Last[0];
+
+            //Обратите внимание, что тип запроса изменился. Теперь он создает последовательность из групп,
+            //имеющих тип char в качестве ключа, и последовательность объектов Student.
+
+            foreach (var studentGroup in studentQuery2)
+            {
+                Console.WriteLine(studentGroup.Key);
+                foreach (Student student in studentGroup)
+                {
+                    Console.WriteLine(" {0}, {1}",
+                    student.Last, student.First);
+                }
+            }
         }
+
+        private static void Ex3()
+        {
+            var studentQuery3 =
+                from student in students
+                group student by student.Last[0];
+
+            foreach (var groupOfStudents in studentQuery3)
+            {
+                Console.WriteLine(groupOfStudents.Key);
+                foreach (var student in groupOfStudents)
+                {
+                    Console.WriteLine(" {0}, {1}",
+                    student.Last, student.First);
+                }
+            }
+
+            Console.WriteLine("----- Uporiadochivanie -----");
+            //Упорядочение групп по значению их ключа:
+
+            var studentQuery4 =
+                from student in students
+                group student by student.Last[0] into studentGroup
+                orderby studentGroup.Key
+                select studentGroup;
+
+            foreach (var groupOfStudents in studentQuery4)
+            {
+                Console.WriteLine(groupOfStudents.Key);
+                foreach (var student in groupOfStudents)
+                {
+                    Console.WriteLine(" {0}, {1}",
+                    student.Last, student.First);
+                }
+            }
+        }
+
     }
 
     public class Student
